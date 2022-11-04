4189db524ed4921411585009601a355b208069d0
Juan Carlos Guillen <jguillen@applaudostudios.com>
2022-11-03 21:14:30 -0500

    ✨ EDC-M03-C05 - Add FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Juan Carlos Guillén Castro on 3/11/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
