7c5bd7d44cb96946b61a096bd498ba5dd77c3cda
Juan Guillen <juan.cgc276@gmail.com>
2022-12-10 01:23:55 -0500

    ✅ EDC-M04-C02 - Add test case RemoteFeedLoader doest not request data upon creation

---
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,26 @@
+//
+//  RemoteFeedLoaderTests.swift
+//  EssentialFeedTests
+//
+//  Created by Juan Carlos Guillén Castro on 10/12/22.
+//
+
+import XCTest
+
+class RemoteFeedLoader {
+    
+}
+
+class HTTPClient {
+    var requestedURL: URL?
+}
+
+class RemoteFeedLoaderTests: XCTestCase {
+    
+    func test_init_doesNotRequestDataFromURL() {
+        let client = HTTPClient()
+        _ = RemoteFeedLoader()
+        
+        XCTAssertNil(client.requestedURL)
+    }
+}
